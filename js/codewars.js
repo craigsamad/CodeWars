cd9576d1390e2bce12af5fe47674147293700690
Craig Samad <samadca@hotmail.com>
2019-11-19 08:05:43 -0500

    more codewars

--- js/codewars.js
+++ js/codewars.js
@@ -1,3 +1,24 @@
+// You are going to be given a word. Your job is to return the middle character of the word. 
+// If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s) {
+    let position;
+    let length;
+    if ((s.length % 2) == 0) {
+      position = s.length / 2 - 1;
+      length = 2;
+    } else {
+      position = (s.length - 1) / 2;
+      length = 1;
+    }
+    
+    return s.substring(position, position + length);
+  }
+  
+  console.log(getMiddle("test")); // es
+  console.log(getMiddle("testing")); // t
+  console.log(getMiddle("middle")); // dd
+
 // You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
 // The returned value must be a string, and have "***" between each of its letters.
 // You should not remove or add elements from/to the array.
