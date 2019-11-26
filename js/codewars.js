872bb6d0101003e64777a0791d94d44f1ee7bf22
Craig Samad <samadca@hotmail.com>
2019-11-26 11:31:54 -0500

    more codewars

--- js/codewars.js
+++ js/codewars.js
@@ -1,3 +1,19 @@
+// Define a function that takes an integer argument and returns logical value true or false 
+// depending on if the integer is a prime.
+// Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive 
+// divisors other than 1 and itself.
+
+function isPrime(num) {
+    let prime = true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i == 0) {
+            prime = false;
+            break;
+        }
+    }
+    return prime && (num > 1);
+}
+
 // You are going to be given a word. Your job is to return the middle character of the word. 
 // If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
